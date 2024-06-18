3f6945a9e1fbeba1778f093900320a169fdb55ba
Jason Schleifer <jason.schleifer@gmail.com>
2024-06-18 12:32:52 +1200

    added new ParallelImageImageQueryTask node

--- nodes/tasks.py
+++ nodes/tasks.py
@@ -3,6 +3,7 @@ import os
 from textwrap import dedent
 
 import folder_paths
+from griptape.artifacts import BaseArtifact, TextArtifact
 from griptape.drivers import (
     AmazonBedrockImageQueryDriver,
     AnthropicImageQueryDriver,
@@ -15,8 +16,9 @@ from griptape.engines import (
     VariationImageGenerationEngine,
 )
 from griptape.loaders import ImageLoader
-from griptape.structures import Agent
+from griptape.structures import Agent, Workflow
 from griptape.tasks import (
+    CodeExecutionTask,
     CsvExtractionTask,
     ImageQueryTask,
     JsonExtractionTask,
@@ -300,6 +302,98 @@ class gtUIImageQueryTask(gtUIBaseImageTask):
             except Exception as e:
                 print(e)
             result = agent.run()
+            output = result.output_task.output.value
+        else:
+            output = "No image provided"
+        return (output, agent)
+
+
+def do_start_task(task: CodeExecutionTask) -> BaseArtifact:
+    return TextArtifact(str(task.input))
+
+
+class gtUIParallelImageQueryTask(gtUIBaseImageTask):
+    CATEGORY = "Griptape/Images"
+
+    # @classmethod
+    # def INPUT_TYPES(cls):
+    #     inputs = super().INPUT_TYPES()
+    #     inputs["optional"].update(
+    #         {
+    #             "rulesets": ("RULESET", {"forceInput": True}),
+    #         }
+    #     )
+    #     # del inputs["optional"]["agent"]
+    #     return inputs
+
+    # RETURN_TYPES = ("STRING", "AGENT")
+    # RETURN_NAMES = ("OUTPUT", "agent")
+
+    def run(
+        self,
+        STRING,
+        image,
+        input_string=None,
+        agent=None,
+    ):
+        final_image = convert_tensor_to_base_64(image)
+        if final_image:
+            if not agent:
+                agent = Agent()
+
+            image_query_driver = agent.config.image_query_driver
+            rulesets = agent.rulesets
+            # If the driver is a DummyImageQueryDriver we'll return a nice error message
+            if isinstance(image_query_driver, DummyImageQueryDriver):
+                return (
+                    dedent("""
+                    I'm sorry, this agent doesn't have access to a valid ImageQueryDriver.
+                    You might want to try using a different Agent Configuration.
+
+                    Reach out for help on Discord (https://discord.gg/gnWRz88eym) if you would like some help.
+                    """),
+                    agent,
+                )
+            engine = ImageQueryEngine(image_query_driver=image_query_driver)
+            image_artifact = ImageLoader().load(base64.b64decode(final_image))
+
+            prompt_text = self.get_prompt_text(STRING, input_string)
+
+            # If the driver is AmazonBedrock or Anthropic, the prompt_text cannot be empty
+            if prompt_text.strip() == "":
+                if isinstance(
+                    image_query_driver,
+                    (AmazonBedrockImageQueryDriver, AnthropicImageQueryDriver),
+                ):
+                    prompt_text = "Describe this image"
+
+            structure = Workflow(rulesets=rulesets)
+            start_task = CodeExecutionTask("Start", run_fn=do_start_task, id="START")
+            # start_task = PromptTask(
+            #     "This is a start task.",
+            #     id="START",
+            #     prompt_driver=agent.config.prompt_driver,
+            # )
+            end_task = PromptTask(
+                "Concatenate just the output values of the tasks, separated by two newlines: {{ parent_outputs }}",
+                id="END",
+                prompt_driver=agent.config.prompt_driver,
+                rulesets=rulesets,
+            )
+            structure.add_task(start_task)
+            structure.add_task(end_task)
+
+            prompts = prompt_text.split("\n")
+            prompt_tasks = []
+            for prompt in prompts:
+                # task = ToolTask(tool=ImageQueryClient(off_prompt=True), rulesets=rulesets)
+                task = ImageQueryTask(
+                    input=(prompt, [image_artifact]), image_query_engine=engine
+                )
+                prompt_tasks.append(task)
+
+            structure.insert_tasks(start_task, prompt_tasks, end_task)
+            result = structure.run()
             output = result.output_task.output.value
         else:
             output = "No image provided"
