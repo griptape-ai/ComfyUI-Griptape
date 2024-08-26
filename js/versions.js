26a1e41290d9763c80e73e06e7104ecca6bbebe7
Jason Schleifer <jason.schleifer@gmail.com>
2024-08-27 11:19:53 +1200

    created a version js so we can track versions

---
+++ js/versions.js
@@ -0,0 +1,18 @@
+export const versions = {
+  "version": "0.27.2a",
+  "releaseDate": "2024-08-21",
+  "name": "ComfyUI-Griptape",
+  "description": "Griptape integration for ComfyUI",
+  "author": "Jason Schleifer",
+  "repository": "https://github.com/griptape-ai/ComfyUI-Griptape",
+  "changelog": [
+    {
+      "version": "0.27.2a",
+      "date": "2024-08-21",
+      "changes": [
+        "Fixed querying for models in LMStudio and Ollama on import"
+      ]
+    }
+  ]
+}
+
