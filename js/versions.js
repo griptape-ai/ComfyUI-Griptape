615ca5fe535970dbcec4caabe36c22798e729c38
GitHub Action <action@github.com>
2024-08-26 23:36:41 +0000

    Update version to

--- js/versions.js
+++ js/versions.js
@@ -1,13 +1,20 @@
 export const versions = {
-  "version": "0.27.2a",
-  "releaseDate": "2024-08-21",
+  "version": "v0.29.2b",
+  "releaseDate": "2024-08-26",
   "name": "ComfyUI-Griptape",
   "description": "Griptape integration for ComfyUI",
   "author": "Jason Schleifer",
   "repository": "https://github.com/griptape-ai/ComfyUI-Griptape",
   "changelog": [
     {
-      "version": "0.27.2a",
+      "version": "v0.29.2b",
+      "date": "2024-08-26",
+      "changes": [
+        "New release"
+      ]
+    },
+    {
+      "version": "v0.29.2b",
       "date": "2024-08-21",
       "changes": [
         "Fixed querying for models in LMStudio and Ollama on import"
